5de07e60822a6a918b29566678e41e47908561f2
Damian Sieroń <damiansieron94@gmail.com>
2018-10-03 09:27:34 +0200

    [rm] Remove method to calculate energy from Premature Ventricular Contractions

--- src/main/java/parameters/PrematureVentricularContractions.java
+++ src/main/java/parameters/PrematureVentricularContractions.java
@@ -9,18 +9,6 @@ import java.util.List;
 
 public class PrematureVentricularContractions {
 
-    public static float[] energy(float[] input) {
-        int windowSize = 100 / (int) Math.toMillisecond(Math.SAMPLING_PERIOD);
-        input = Normalization.normalize(input);
-        float[] output = new float[input.length - windowSize];
-        for (int i = 0; i < input.length - windowSize; i++) {
-            for (int j = i; j < i + windowSize; j++) {
-                output[i] += input[j] * input[j];
-            }
-        }
-        return output;
-    }
-
     private static float prematurity(float intervalRR, float averageIntervalRR) {
         return (averageIntervalRR - intervalRR) / averageIntervalRR;
     }
